d5dd2543eb2c5d7bc14e9d373f611df71b3ca158
NeoDriveinfo <NeoDrive.info@gmail.com>
2025-11-03 13:34:20 +0100

    Delete vite.contig.ts

--- vite.contig.ts
+++
@@ -1,9 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ¡Importante! base = '/<nombre-del-repo>/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/neodrive-site/',
-})
